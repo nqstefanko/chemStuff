8647aae54bb20e47e0a90faf27093ba6a63f45d3
nqstefanko <nqstefanko@gmail.com>
2020-06-17 10:31:00 -0700

    Update chem_util.py

--- chem_util.py
+++ chem_util.py
@@ -1,14 +1,22 @@
 import periodictable as pt
 import os
+from decimal import Decimal
 
 water = pt.H.mass * 2 + pt.O.mass
+ammonia = pt.N.mass + (4 * pt.H.mass)
 co2 = pt.C.mass + (2 * pt.O.mass)
 hcl = pt.H.mass + pt.Cl.mass
 nacl = pt.Na.mass + pt.Cl.mass
 nitrate = pt.N.mass + 3 * pt.O.mass
 sulfate = pt.S.mass + 3 * pt.O.mass
 
-r = .08206
+ava = 6.022e23
+r = .0821 # ideal gas constant
+c = 3e8 # m/s Speed of Light
+h = 6.62607004e-34 #m2 kg / s planks constant
+
+def sn(num):
+	print(f"{Decimal(num):.5E}")
 
 def K(c):
 	return c + 273
@@ -38,3 +46,4 @@ def pvnrt(P, V, n, T):
 
 def pe(r, t):
 	return abs((r-t)/t * 100)
+
